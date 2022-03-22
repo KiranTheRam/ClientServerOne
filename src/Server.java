33259ec31b2cf362229a7485c5fcc5c868bd8dcf
KiranTheRam <kramji12@gmail.com>
2022-03-22 14:01:49 -0400

    Fixed timestamp not updating

--- src/Server.java
+++ src/Server.java
@@ -11,13 +11,15 @@ public class Server {
         Socket socket = serverSocket.accept();
 
 //        So we know a client has connected
-        System.out.println(new Timestamp(date.getTime()) + "[Server] Client has connected");
+        date = new Date();
+        System.out.println(new Timestamp(date.getTime()) + " [Server] Client has connected");
 
 //        Convert message sent by client and display it on server
         InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
         String message = bufferedReader.readLine();
-        System.out.println(new Timestamp(date.getTime()) + "[Client]"+ message);
+        date = new Date();
+        System.out.println(new Timestamp(date.getTime()) +" "+ message);
 
 //        Send a message back to the client to inform them their message has been received
         PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
