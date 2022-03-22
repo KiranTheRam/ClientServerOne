9c58544e5234feb058943b86bc4985e13c2bef86
KiranTheRam <kramji12@gmail.com>
2022-03-22 01:32:46 -0400

    Initial Commit

---
+++ src/Client.java
@@ -0,0 +1,26 @@
+import java.net.*;
+import java.io.*;
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class Client {
+    public static void main(String[] args) throws IOException{
+        Socket socket = new Socket("localhost", 4999);
+        Date date = new Date();
+
+//        Message client is sending to the server
+        String messageToBeSent = "[Client] Test Message, did the server receive it?";
+//        Displaying to the client what message is about to be sent
+        System.out.println(new Timestamp(date.getTime()) + " Sending the server this message: \"" + messageToBeSent+ "\"");
+//        Sending the message to the server
+        PrintWriter sending = new PrintWriter(socket.getOutputStream(), true);
+        sending.println(messageToBeSent);
+
+//        Receiving message back from the server.
+        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String message = bufferedReader.readLine();
+        System.out.println(new Timestamp(date.getTime()) + message);
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,29 @@
+import java.net.*;
+import java.io.*;
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        Date date = new Date();
+        ServerSocket serverSocket = new ServerSocket(4999);
+        System.out.println(new Timestamp(date.getTime()) + " [Server] Waiting for client to connect ...");
+        Socket socket = serverSocket.accept();
+
+//        So we know a client has connected
+        System.out.println(new Timestamp(date.getTime()) + "[Server] Client has connected");
+
+//        Convert message sent by client and display it on server
+        InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String message = bufferedReader.readLine();
+        System.out.println(new Timestamp(date.getTime()) + "[Client]"+ message);
+
+//        Send a message back to the client to inform them their message has been received
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+        printWriter.println(" [Server] You have successfully connected to the server and your message was received");
+
+
+
+    }
+}
